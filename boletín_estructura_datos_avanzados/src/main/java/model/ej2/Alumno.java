aa9b084377f0d0e3778e3c865803f38b026239c4
psanant1208 <psanant1208@gmail.com>
2025-03-31 20:22:44 +0200

    Ej 3 acabado Alumno.java

--- boletín_estructura_datos_avanzados/src/main/java/model/ej2/Alumno.java
+++ boletín_estructura_datos_avanzados/src/main/java/model/ej2/Alumno.java
@@ -5,11 +5,17 @@ import java.util.Objects;
 public class Alumno {
 	private String nombre;
 	private String dni;
+	private int edad;
+	private char sexo;
+	private String ciudad;
 	
-	public Alumno(String nombre, String dni) {
+	public Alumno(String nombre, String dni, int edad, char sexo, String ciudad) {
 		super();
 		this.nombre = nombre;
 		this.dni = dni;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.ciudad = ciudad;
 	}
 	
 	public String getNombre() {
@@ -26,6 +32,30 @@ public class Alumno {
 
 	public void setDni(String dni) {
 		this.dni = dni;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
 	}
 
 	@Override
